cc709af363f2645e84eb7917822a49162a680e2e
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-08-07 21:57:03 +0300

    fix image

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -11,7 +11,14 @@ export const ProductCard: React.FC<ProductCardProps> = ({ data}) => {
     console.log(data)
     return <div className="mb-6">
         <div className=" aspect-[3/4.5] ">
-            <img className="w-[100%] h-[100%] object-cover" src={data.images[0].url} alt="image" />
+        <Image
+        className="w-[100%] h-[100%] object-cover" src={data.images[0].url}
+
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+            {/* <img className="w-[100%] h-[100%] object-cover" src={data.images[0].url} alt="image" /> */}
         </div>
         <h1>{data.name}</h1>
      
