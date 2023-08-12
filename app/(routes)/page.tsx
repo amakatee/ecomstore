4bb71a372c450a653db4c716e1f87523ecb491d1
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-08-12 18:20:18 +0300

    products fetch

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,45 +4,33 @@ import { Container } from "@/components/ui/container"
 
 import getCategories from "@/actions/get-categories"
 import { Billboard } from "@/components/billboard"
-// import getBillboard from "@/actions/get-billboards"
+import getBillboard from "@/actions/get-billboards"
 import getProducts from "@/actions/get-products"
 import { ProductList } from "@/components/product-list"
-import {Category, BillBoard, Product} from '../../types'
-
- //
-const URL=`${process.env.NEXT_PUBLIC_API_URL}/billboards`;
-
-const getBillboard = async (id: string): Promise<BillBoard> => {
-  const res = await fetch(`${URL}/${id}`);
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json();
-};
+import {Category, BillBoard , Product} from '../../types'
 
 
-//
 
 export default async function HomePage() {
   // const categories = await getCategories()
 
-  // const billboard = await getBillboard("d8af523f-6696-4357-9fd4-52ee9946e190")
-  // const products = await getProducts({isFeatured: true})
+   const billboard: BillBoard = await getBillboard("76f1cd7d-36b3-4967-9235-fda6bbd844d1")
+    const products: Product[] = await getProducts({isFeatured: true})
   // console.log(products)
+  // console.log(products)
+  console.log(billboard)
  
   return (
     <>
     <Navbar categories={[] as Category[]} />
       <main className=" top-0 left-0 w-[100vw] h-[100vh] z-[-1] ">
          <div className="billboard">
-            <Billboard data={{ id: "231",label: "newew", imageUrl: "sjcdjsha"} }/>
+            <Billboard data={billboard}/>
           </div>
           
       </main>
       <div className="w-[100vw] h-[100vh] z-100">
-            <ProductList title="Featured Products" items={[] as Product[]} />
+            <ProductList title="Featured Products" items={products} />
           </div>
     
     {/* <Footer /> */}
