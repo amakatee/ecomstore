bf6423824c4edd157842fc8e44b878c973dfaa31
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-08-12 18:39:27 +0300

    ts

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -15,7 +15,7 @@ export default async function HomePage() {
   const categories: Category[] = await getCategories()
   const billboard: BillBoard = await getBillboard("76f1cd7d-36b3-4967-9235-fda6bbd844d1")
   //const products = await getProducts({isFeatured: true})
-  const products = await getProducts({isFeatured:true})
+  const products: Product[] = await getProducts({isFeatured:true})
 
   console.log(products)
   
