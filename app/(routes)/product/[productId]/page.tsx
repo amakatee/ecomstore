95c2493270be41b9e30fb24fb7781c873a29d4ff
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-08-13 22:25:11 +0300

    product desc

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -7,6 +7,7 @@ import {Category} from '../../../../types'
 import { ImagesSlider } from '@/components/ui/slider'
 import Button from '../../../../components/ui/button'
 import {Currency} from '../../../../components/ui/currency'
+import ProductNavbar from '../../../../components/product-nav'
 interface ProductPageProps {
     params: {
         productId: string
@@ -23,30 +24,54 @@ export default async function ProductPage ({params} : ProductPageProps) {
     // const suggestedProducts = await getProducts({ 
     //     categoryId: product.category.id 
     //   });
-
+    
     
     return <div className='min-h-[100vh]'>
+        <ProductNavbar />
         <Container>
             <div className=' sm:px-6 lg:px-8'>
                 <div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
                 <div className=' h-[75vh]'>
                     <ImagesSlider  images={product.images}/>
                  </div>
-                <div className='flex flex-col gap-3 mt-6 px-7 lg:mt-0'>
-                    <div className='block font-semibold leading-3 tracking-wider uppercase border-box'>
-                      <h1>{product.name}</h1>
+                <div className='flex flex-col gap-3 px-5 mt-6 lg:mt-0'>
+                   <div className='flex flex-col gap-2'>
+                     <div className='block font-semibold leading-3 tracking-wider uppercase border-box'>
+                        <h1>{product.name}</h1>
                     </div>
-                    <div className='text-sm font-bold tracking-wider'>
+                    <div className='mb-3 text-sm font-bold tracking-wider'>
                        <Currency value={product.price} />
                     </div>
+                   </div>
 
-                    <div className='mb-30'>
+                    <div className='pb-5 text-xs font-bold tracking-wide'>
                         {product.description}
                     </div>
-                    <div className='py-10'>
-                    <Button>
-                       Add To Cart
-                   </Button>
+
+
+                    <div className='flex flex-col gap-8'>
+                    
+                    <div className='flex flex-col gap-3 '>
+                    <p className='text-xs font-bold uppercase '>color:</p>
+                        <div className='flex flex-col justify-center gap-1' >
+                            <span style={{ backgroundColor: product?.color?.value }} className={` w-[20px] h-[20px] rounded-full `}></span>
+                            <p className='text-xs font-bold uppercase '>{product.color.name}</p>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col gap-3'>
+                        <p className='text-xs font-bold uppercase'>size:</p>
+                        <div className='flex flex-col justify-center gap-1' >
+
+                            <span className={`bg-[#fafafa] w-[20px] h-[30px] text-center `}> <p className='pt-[2px]'>{product.size.value}</p> </span>
+                        </div>
+                    </div>
+
+                    </div>
+                    <div className='flex justify-center w-[100%] my-10 '>
+                    <button className='border border-black w-[100%] py-3 cursor-pointers'>
+                       <p className='text-xs uppercase fomt-semibold'>Add To Cart</p>
+                   </button>
                     </div>
                     
                    
---
+++ components/product-nav.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Button from "@/components/ui/button";
+// import useCart from "@/hooks/use-cart";
+
+const ProductNavbar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const router = useRouter();
+//   const cart = useCart();
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return ( 
+   
+    <div className="flex justify-between content-center px-6 pt-5 w-[100vw] fixed z-10">
+         <Button onClick={() => router.push('/products')}>
+         <div className="s w-[1.1rem] h-[1.1rem]">
+        <svg viewBox="0 0 19 15" fill="black" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M7.647.146l.707.708L2.207 7H19v1H2.207l6.147 6.146-.707.708L.293 7.5 7.647.146z"></path>
+            </svg>
+
+        </div>
+         </Button>
+        
+        <Button onClick={() => router.push('/cart')}>
+        <div className="shopping-cart">
+            <p className="text-black">0</p>
+            <svg viewBox="0 0 18 26" fill="black" xmlns="http://www.w3.org/2000/svg">
+                 <path fillRule="evenodd" clipRule="evenodd" d="M9 2a4 4 0 00-4 4v1.752A8.502 8.502 0 000 15.5V25h18v-9.5a8.502 8.502 0 00-5-7.748V6a4 4 0 00-4-4zm3 5.374V6a3 3 0 10-6 0v1.374A8.498 8.498 0 018.5 7h1c.87 0 1.71.13 2.5.374zm5 8.126V24H1v-8.5A7.5 7.5 0 018.5 8h1a7.5 7.5 0 017.5 7.5z"></path>
+            </svg>
+                </div>
+        </Button>
+                       
+                    </div>
+  );
+}
+ 
+export default ProductNavbar;
