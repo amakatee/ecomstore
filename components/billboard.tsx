be782816e54feef3128028a4132028a7fd411d53
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-08-18 14:29:04 +0300

    new billboard

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -7,7 +7,7 @@ interface BillboardProps {
 export const Billboard: React.FC<BillboardProps> = ({data}) => {
     return (
         <div className="">
-            {/* <div
+            <div
              className="h-[100vh] w-[100vw]"
              style={{backgroundImage: `url(${data?.imageUrl})`}}
               >
@@ -27,8 +27,7 @@ export const Billboard: React.FC<BillboardProps> = ({data}) => {
                     
                 </div>
   
-             </div> */}
-             bb
+             </div>
         </div>
     )
 }
